9d636e2ca76b15df7aa08ca4d25d091017a44807
Ronaiza Cardoso <ronaizacard@gmail.com>
2018-11-04 17:36:07 +0000

    make it react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Frontend Developer</h1>
+        <p>
+          Hi, I'm Ronaiza.<br/>
+          I’m a Frontend Developer trying to make some JSON data seems more readable for humans. Currently in love with <strong>Reactjs</strong>.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
