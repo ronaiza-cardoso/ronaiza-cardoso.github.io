fd55896d290f0a5c00ac19a7884e81e792078c77
Ronaiza Cardoso <ronaizacard@gmail.com>
2018-11-05 14:22:57 +0000

    make it react

---
+++ precache-manifest.2e53a0668062234a71e3b3aca6a4041c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "73a51b6e056c64d2dfb4",
+    "url": "/static/js/main.73a51b6e.chunk.js"
+  },
+  {
+    "revision": "39dbcc1e0251abf6b421",
+    "url": "/static/js/1.39dbcc1e.chunk.js"
+  },
+  {
+    "revision": "73a51b6e056c64d2dfb4",
+    "url": "/static/css/main.5fb4c6c9.chunk.css"
+  },
+  {
+    "revision": "5d830666199ac752eda6a902ab300ffd",
+    "url": "/index.html"
+  }
+];
